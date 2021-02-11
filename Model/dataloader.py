a3c70ffc6153e30a079d48109eb7666d3da209e4
Shoumik-20 <55137590+Shoumik-20@users.noreply.github.com>
2021-02-11 23:04:50 +0530

    dataloader for speechcommand
    Currently using inbuilt dataset

---
+++ Model/dataloader.py
@@ -0,0 +1,26 @@
+import warnings
+warnings.filterwarnings("ignore")
+import os
+import torch
+import torchaudio
+from torchaudio.datasets import SPEECHCOMMANDS
+
+torchaudio.set_audio_backend("soundfile")
+
+class SubsetSC(SPEECHCOMMANDS):
+    def __init__(self, subset: str = None):
+        super().__init__("./", download=True)
+
+        def load_list(filename):
+            filepath = os.path.join(self._path, filename)
+            with open(filepath) as fileobj:
+                return [os.path.join(self._path, line.strip()) for line in fileobj]
+
+        if subset == "validation":
+            self._walker = load_list("validation_list.txt")
+        elif subset == "testing":
+            self._walker = load_list("testing_list.txt")
+        elif subset == "training":
+            excludes = load_list("validation_list.txt") + load_list("testing_list.txt")
+            excludes = set(excludes)
+            self._walker = [w for w in self._walker if w not in excludes]
